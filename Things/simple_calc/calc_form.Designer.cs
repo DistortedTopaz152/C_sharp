58fa04bd4aa4f64732542571a5502828cc1cb128
Dyson Smith <dyson.smith@tooeleschools.org>
2021-09-27 14:16:02 -0600

    class work

---
+++ Things/simple_calc/calc_form.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace simpleCalc
+{
+    partial class calc_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output_txb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.addition_bttn = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.subtraction_bttn = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.multiply_bttn = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.divition_btttn = new System.Windows.Forms.Button();
+            this.submit_bttn = new System.Windows.Forms.Button();
+            this.clear_bttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // output_txb
+            // 
+            this.output_txb.Font = new System.Drawing.Font("Perpetua Titling MT", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output_txb.Location = new System.Drawing.Point(11, 12);
+            this.output_txb.Name = "output_txb";
+            this.output_txb.ReadOnly = true;
+            this.output_txb.Size = new System.Drawing.Size(259, 64);
+            this.output_txb.TabIndex = 0;
+            this.output_txb.Text = "0";
+            this.output_txb.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(78, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 56);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // addition_bttn
+            // 
+            this.addition_bttn.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addition_bttn.Location = new System.Drawing.Point(210, 82);
+            this.addition_bttn.Name = "addition_bttn";
+            this.addition_bttn.Size = new System.Drawing.Size(61, 56);
+            this.addition_bttn.TabIndex = 4;
+            this.addition_bttn.Text = "+";
+            this.addition_bttn.UseVisualStyleBackColor = true;
+            this.addition_bttn.Click += new System.EventHandler(this.addition_bttn_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(144, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 56);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(78, 144);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(61, 56);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(12, 144);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(61, 56);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // subtraction_bttn
+            // 
+            this.subtraction_bttn.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtraction_bttn.Location = new System.Drawing.Point(210, 144);
+            this.subtraction_bttn.Name = "subtraction_bttn";
+            this.subtraction_bttn.Size = new System.Drawing.Size(61, 56);
+            this.subtraction_bttn.TabIndex = 8;
+            this.subtraction_bttn.Text = "-";
+            this.subtraction_bttn.UseVisualStyleBackColor = true;
+            this.subtraction_bttn.Click += new System.EventHandler(this.subtraction_bttn_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(144, 144);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(61, 56);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(78, 206);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(61, 56);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(12, 206);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(61, 56);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // multiply_bttn
+            // 
+            this.multiply_bttn.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiply_bttn.Location = new System.Drawing.Point(210, 206);
+            this.multiply_bttn.Name = "multiply_bttn";
+            this.multiply_bttn.Size = new System.Drawing.Size(61, 56);
+            this.multiply_bttn.TabIndex = 12;
+            this.multiply_bttn.Text = "*";
+            this.multiply_bttn.UseVisualStyleBackColor = true;
+            this.multiply_bttn.Click += new System.EventHandler(this.multiply_bttn_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(144, 206);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(61, 56);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button0
+            // 
+            this.button0.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button0.Location = new System.Drawing.Point(78, 268);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(61, 56);
+            this.button0.TabIndex = 14;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // divition_btttn
+            // 
+            this.divition_btttn.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divition_btttn.Location = new System.Drawing.Point(210, 268);
+            this.divition_btttn.Name = "divition_btttn";
+            this.divition_btttn.Size = new System.Drawing.Size(60, 56);
+            this.divition_btttn.TabIndex = 16;
+            this.divition_btttn.Text = "/";
+            this.divition_btttn.UseVisualStyleBackColor = true;
+            this.divition_btttn.Click += new System.EventHandler(this.divition_btttn_Click);
+            // 
+            // submit_bttn
+            // 
+            this.submit_bttn.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submit_bttn.Location = new System.Drawing.Point(144, 268);
+            this.submit_bttn.Name = "submit_bttn";
+            this.submit_bttn.Size = new System.Drawing.Size(60, 56);
+            this.submit_bttn.TabIndex = 15;
+            this.submit_bttn.Text = "=";
+            this.submit_bttn.UseVisualStyleBackColor = true;
+            this.submit_bttn.Click += new System.EventHandler(this.submit_bttn_Click);
+            // 
+            // clear_bttn
+            // 
+            this.clear_bttn.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear_bttn.Location = new System.Drawing.Point(12, 268);
+            this.clear_bttn.Name = "clear_bttn";
+            this.clear_bttn.Size = new System.Drawing.Size(61, 56);
+            this.clear_bttn.TabIndex = 13;
+            this.clear_bttn.Text = "C";
+            this.clear_bttn.UseVisualStyleBackColor = true;
+            this.clear_bttn.Click += new System.EventHandler(this.clear_bttn_Click);
+            // 
+            // calc_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 340);
+            this.Controls.Add(this.divition_btttn);
+            this.Controls.Add(this.submit_bttn);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.clear_bttn);
+            this.Controls.Add(this.multiply_bttn);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.subtraction_bttn);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.addition_bttn);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.output_txb);
+            this.Name = "calc_form";
+            this.Text = "My Calc";
+            this.Load += new System.EventHandler(this.calc_form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox output_txb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button addition_bttn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button subtraction_bttn;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button multiply_bttn;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button divition_btttn;
+        private System.Windows.Forms.Button submit_bttn;
+        private System.Windows.Forms.Button clear_bttn;
+    }
+}
+
---
+++ Things/simple_calc/calc_form.cs
@@ -0,0 +1,248 @@
+﻿/*
+ Dyson Smith
+ Calculator
+ Sept. 27 2021
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simpleCalc
+{
+    public partial class calc_form : Form
+    {
+        String output;
+        int operand1;
+        int operand2;
+        String opp;
+        public calc_form()
+        {
+            InitializeComponent();
+        }
+
+        private void calc_form_Load(object sender, EventArgs e)
+        {
+            operand1 = 0;
+            operand2 = 0;
+            opp = "";
+            output = "0";
+            output_txb.Text = output;
+            //adding text to buttons
+            button0.Text = "0";
+            button1.Text = "1";
+            button2.Text = "2";
+            button3.Text = "3";
+            button4.Text = "4";
+            button5.Text = "5";
+            button6.Text = "6";
+            button7.Text = "7"; 
+            button8.Text = "8";
+            button9.Text = "9";
+            addition_bttn.Text = "+";
+            subtraction_bttn.Text = "-";
+            multiply_bttn.Text = "*";
+            divition_btttn.Text = "/";
+            submit_bttn.Text = "=";
+            clear_bttn.Text = "C";
+
+            //set font properties
+            Font font = new Font("Perpetua Titling MT", 28, FontStyle.Bold);
+            button0.Font = font;
+            button1.Font = font;
+            button2.Font = font;
+            button3.Font = font;
+            button4.Font = font;
+            button5.Font = font;
+            button6.Font = font;
+            button7.Font = font;
+            button8.Font = font;
+            button9.Font = font;
+            addition_bttn.Font = font;
+            subtraction_bttn.Font = font;
+            multiply_bttn.Font = font;
+            divition_btttn.Font = font;
+            submit_bttn.Font = font;
+            clear_bttn.Font = font;
+
+            output_txb.Font = new Font("Perpetua Titling MT", 36);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (output == "0")
+            {
+                output = "1";
+            }
+            else
+            {
+                output = output + "1";
+            }
+            output_txb.Text = output;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (output == "0")
+            {
+                output = "2";
+            }
+            else
+            {
+                output = output + "2";
+            }
+            output_txb.Text = output;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (output == "0")
+            {
+                output = "3";
+            }
+            else
+            {
+                output = output + "3";
+            }
+            output_txb.Text = output;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (output == "0")
+            {
+                output = "4";
+            }
+            else
+            {
+                output = output + "4";
+            }
+            output_txb.Text = output;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (output == "0")
+            {
+                output = "5";
+            }
+            else
+            {
+                output = output + "5";
+            }
+            output_txb.Text = output;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (output == "0")
+            {
+                output = "6";
+            }
+            else
+            {
+                output = output + "6";
+            }
+            output_txb.Text = output;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (output == "0")
+            {
+                output = "7";
+            }
+            else
+            {
+                output = output + "7";
+            }
+            output_txb.Text = output;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (output == "0")
+            {
+                output = "8";
+            }
+            else
+            {
+                output = output + "8";
+            }
+            output_txb.Text = output;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (output == "0")
+            {
+                output = "9";
+            }
+            else
+            {
+                output = output + "9";
+            }
+            output_txb.Text = output;
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (output == "0")
+            {
+                output = "0";
+            }
+            else
+            {
+                output = output + "0";
+            }
+            output_txb.Text = output;
+        }
+
+        private void addition_bttn_Click(object sender, EventArgs e)
+        {
+            operand1 = int.Parse(output);
+            opp = "+";
+            output = "0";
+            output_txb.Text = output;
+        }
+
+        private void subtraction_bttn_Click(object sender, EventArgs e)
+        {
+            operand1 = int.Parse(output);
+            opp = "-";
+            output = "0";
+            output_txb.Text = output;
+        }
+
+        private void multiply_bttn_Click(object sender, EventArgs e)
+        {
+            operand1 = int.Parse(output);
+            opp = "*";
+            output = "0";
+            output_txb.Text = output;
+        }
+
+        private void divition_btttn_Click(object sender, EventArgs e)
+        {
+            operand1 = int.Parse(output);
+            opp = "/";
+            output = "0";
+            output_txb.Text = output;
+        }
+
+        private void submit_bttn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear_bttn_Click(object sender, EventArgs e)
+        {
+            output = "0";
+        }
+    }
+}
